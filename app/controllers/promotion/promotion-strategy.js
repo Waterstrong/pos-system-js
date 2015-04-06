e4fb0073827eca2b492df5170e5a9255c42ab65b
Waterstrong <sqlin@thoughtworks.com>
2015-04-06 13:53:50 +0800

    [Waterstrong]: add promotion-strategy attach

--- app/controllers/promotion/promotion-strategy.js
+++ app/controllers/promotion/promotion-strategy.js
@@ -4,18 +4,27 @@
 
 var instance = require('../../utils/instance');
 
-var promotionMapper = {
-    barcode: {
-        promotions: []
-    }
+var self = {
+    promotionsMapper: {}
 };
+
 module.exports = {
+    attach: function(promotionList) {
+        if(!promotionList) return;
+        promotionList.forEach(function(promotion) {
+            var existPromotions = self.promotionsMapper[promotion.barcode];
+            if(!existPromotions) {
+                self.promotionsMapper[promotion.barcode] = [];
+            }
+            self.promotionsMapper[promotion.barcode].push(promotion);
+        });
+    },
     calculate: function(item) {
         if(!item) {
             console.log("promotion-strategy: item is undefined.");
             return undefined;
         }
-        var promotions = promotionMapper[item.barcode];
+        var promotions = self.promotionsMapper[item.barcode];
         var newItem = instance.clone(item); // TODO: how to solve the ref problem
         if(promotions) {
             promotions.forEach(function(promotion){
